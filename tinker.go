efe9ee705d64217c4215d4c9f98605b6f147788a
Wulfheart <dev@alexfwulf.de>
2020-06-20 14:25:43 +0200

    Gradient generation working

--- tinker.go
+++ tinker.go
@@ -1,46 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"os"
 	"wulfheart/brahms/viz"
 )
 
 func main(){
-	keypoints := []string{
-		"#40E0D0", "#FF8C00", "#FF0080",
-	}
-
-	palette, err := viz.MakePalette(10, keypoints...)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, p := range palette {
-		fmt.Println(p.Hex())
-	}
-	// h := 1024
-	// w := 40
-	//
 	// keypoints := []string{
 	// 	"#40E0D0", "#FF8C00", "#FF0080",
 	// }
 	//
-	// img := image.NewRGBA(image.Rect(0, 0, w, h))
-	//
-	// palette, err := viz.MakePalette(h, keypoints...)
+	// palette, err := viz.MakePalette(5, keypoints...)
 	// if err != nil {
 	// 	panic(err)
 	// }
+	// fmt.Println(len(palette))
 	//
-	// for y := 0; y < h -1; y++ {
-	// 	draw.Draw(img, image.Rect(0, y, w, y + 1), &image.Uniform{C: palette[y]}, image.Point{},draw.Src)
+	// for _, p := range palette {
+	// 	fmt.Println(p.Hex())
 	// }
-	//
-	// outpng, err := os.Create("gradientgen.png")
-	// if err != nil {
-	// 	panic("Error storing png: " + err.Error())
-	// }
-	// defer outpng.Close()
-	//
-	// png.Encode(outpng, img)
+	h := 1024
+	w := 40
+
+	keypoints := []string{
+		"#40E0D0", "#FF8C00", "#FF0080",
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
+
+	palette, err := viz.MakePalette(h, keypoints...)
+	if err != nil {
+		panic(err)
+	}
+
+	for y := 0; y < h -1; y++ {
+		draw.Draw(img, image.Rect(0, y, w, y + 1), &image.Uniform{C: palette[y]}, image.Point{},draw.Src)
+	}
+
+	outpng, err := os.Create("gradientgen.png")
+	if err != nil {
+		panic("Error storing png: " + err.Error())
+	}
+	defer outpng.Close()
+
+	png.Encode(outpng, img)
 }
--- viz/palette.go
+++ viz/palette.go
@@ -14,29 +14,28 @@ func MakePalette(steps int, hex ...string) (pc []colorful.Color, err error) {
 		}
 		colors = append(colors, c)
 	}
+
 	colors, err = MakeGradient(colors)
 	if err != nil {
 		panic(err)
 	}
-	stepSize := float64(len(colors))/float64(steps)
+	fmt.Println(len(colors))
+	stepSize := float64(len(colors) - 1)/float64(steps - 1)
 	for i := 0 ; i < steps; i++ {
 		index := int(float64(i) * stepSize)
-		if index > len(colors) {
+		if index > len(colors) - 1 {
 			return nil, fmt.Errorf("calculated colorindex out of range")
 		}
 		pc = append(pc, colors[index])
 	}
 	return pc, nil
-
-
-
-	// return colors
 }
 
 func MakeGradient(colors []colorful.Color) (rc []colorful.Color, err error) {
 	for i := 0; i < len(colors)-1; i++ {
-		for j := 1; j < 100; j++ {
-			rc = append(rc, colors[i].BlendHcl(colors[i+1], float64(i)/float64(j)))
+		steps := 1000
+		for j := 1; j <= steps; j++ {
+			rc = append(rc, colors[i].BlendHcl(colors[i+1], float64(j)/float64(steps - 1)).Clamped())
 		}
 	}
 	return rc, nil
